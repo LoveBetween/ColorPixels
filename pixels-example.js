f951bad4ed8659266c21172a5a77de4495d85b4d
LoveBetween <39136142+LoveBetween@users.noreply.github.com>
2023-11-04 10:47:22 +0100

    Update pixels-example.js
    added greyscale and seed visualizer

--- pixels-example.js
+++ pixels-example.js
@@ -7,15 +7,29 @@ window.onload = function() {
     var widthSlider = document.getElementById("width");
     var heightSlider = document.getElementById("height");
     var osSlider = document.getElementById("myRange");
+    let span = document.getElementById("firstSpan");
     document.getElementById ("gen").addEventListener ("click", generate, false);
     document.getElementById ("toggleShuffle").addEventListener ("click", toggleShuffle, false);
+    document.getElementById ("toggleGreyScale").addEventListener ("click", toggleGreyScale, false);
     osSlider.addEventListener ("change", setOffset, false);
+
     
     // Define the image dimensions
     var shuffle = false;
+    var greyscale = false;
     var shuffleCycle = 10;
     var width;
     var height;
+    var directions = {
+        SW: [-1,1],
+        NE: [1,-1],
+        E: [1,0],
+        SE: [1,1],
+        N: [0,-1],
+        S: [0,1],
+        NW: [-1,-1],
+        W: [-1,0]
+    }
     var rdir = [
         [-1,1],
         [1,-1],
@@ -29,16 +43,28 @@ window.onload = function() {
     var bitmap;
     var count;
     var running = false;
-    var os = osSlider.value;
+    var os = Math.sqrt(osSlider.value);
     var imagedata;// Create an ImageData object
     
-    // ------------------------------------------------------- // buttons
+    // ------------------------------------------------------- // UI
     function toggleShuffle(){
         shuffle = !shuffle;
         console.log("toggling shuffle");
     }
+    function toggleGreyScale(){
+        greyscale = !greyscale;
+    }
     
-
+    function displayDegrees(){
+         span.textContent = "seed: "+printArr(rdir);
+    }
+    function printArr(arr) {
+        let str = "";
+          for (let item of arr) {
+            str+="["+item[0]+","+item[1]+"];";
+          }
+          return str.slice(0,-1);
+        }
     // ------------------------------------------------------- //
     
     function generate(){
@@ -46,6 +72,7 @@ window.onload = function() {
         createImage();
         setup();
         running = true;
+        displayDegrees();
         main(0);
     }
 
@@ -57,7 +84,7 @@ window.onload = function() {
     }
 
     function setOffset(){
-        os = (osSlider.value * osSlider.value)/4;
+        os = Math.sqrt(osSlider.value);
     }
     // Create the image
     function createImage() {
@@ -97,8 +124,12 @@ window.onload = function() {
         imagedata.data[index+1] =  Math.floor(Math.random() * 255); // Green
         imagedata.data[index+2] =  Math.floor(Math.random() * 255); // Blue
         imagedata.data[index+3] = 255;// Apha
-        
+        if(greyscale==true){
+            imagedata.data[index+1] =  imagedata.data[index];
+            imagedata.data[index+2] =  imagedata.data[index];
+        }
     }
+
     function addPixel(index, newIndex, yOffset, xOffset, count){
         if(Math.random() > 0.5){
             imagedata.data[newIndex] = imagedata.data[index] + Math.ceil(Math.random() * 2*os -os); // Red
@@ -109,6 +140,23 @@ window.onload = function() {
             imagedata.data[newIndex] = imagedata.data[index] + Math.floor(Math.random() * 2*os -os); // Red
             imagedata.data[newIndex+1] = imagedata.data[index+1] + Math.floor( Math.random() *  2*os -os); // Green
             imagedata.data[newIndex+2] = imagedata.data[index+2] + Math.floor(Math.random() *  2*os -os); // Blue
+        }
+        imagedata.data[newIndex+3] = 255; // Alpha
+        bitmap[count*3] = xOffset;
+        bitmap[count*3 +1] = yOffset;
+        bitmap[count*3 +2] = 1;
+    }
+
+    function addGreyPixel(index, newIndex, yOffset, xOffset, count){
+        if(Math.random() > 0.5){
+            imagedata.data[newIndex] = imagedata.data[index] + Math.ceil(Math.random() * 2*os -os); // Red
+            imagedata.data[newIndex+1] = imagedata.data[newIndex]; // Green
+            imagedata.data[newIndex+2] = imagedata.data[newIndex]; // Blue
+        }
+        else {
+            imagedata.data[newIndex] = imagedata.data[index] + Math.floor(Math.random() * 2*os -os); // Red
+            imagedata.data[newIndex+1] = imagedata.data[newIndex]; // Green
+            imagedata.data[newIndex+2] = imagedata.data[newIndex]; // Blue
         }
         imagedata.data[newIndex+3] = 255; // Alpha
         bitmap[count*3] = xOffset;
@@ -140,7 +188,12 @@ window.onload = function() {
                     let newIndex = ( yOffset * width + xOffset ) * 4;
                     
                     if(xOffset<width && xOffset>-1 && imagedata.data[newIndex+3] == 0){
-                        addPixel(index,newIndex,yOffset,xOffset, count)
+                        if(greyscale==true){
+                            addGreyPixel(index,newIndex,yOffset,xOffset, count);
+                        }
+                        else{
+                            addPixel(index,newIndex,yOffset,xOffset, count);
+                        }
                         count++;
                         added = true;
                         break;
